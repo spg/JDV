56ab0ccfbecfcef83da2a6212c302af09322645b
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-02-06 14:53:01 -0500

    Corrected server/client communication

--- python/Client.py
+++ python/Client.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*
 import socket
 
 hote = "10.240.230.197" #adresse ip du mac mini
@@ -9,13 +10,13 @@ print "Connexion établie avec le serveur sur le port", port
 
 msg_a_envoyer = b""
 while msg_a_envoyer != b"fin":
-    msg_a_envoyer = input("> ")
+    msg_a_envoyer = raw_input("> ")
     # Peut planter si vous entrez des caractères spéciaux
     msg_a_envoyer = msg_a_envoyer.encode()
     # On envoie le message
     connexion_avec_serveur.send(msg_a_envoyer)
     msg_recu = connexion_avec_serveur.recv(1024)
-    print(msg_recu.decode()) # là encore, peut planter si y'a des accents
+    print msg_recu.decode() # là encore, peut planter si y'a des accents
 
-print("Fermeture de la connexion")
+print "Fermeture de la connexion"
 connexion_avec_serveur.close()
--- python/Server.py
+++ python/Server.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*
 import socket
 
 hote = ''
@@ -6,7 +7,7 @@ port = 12800
 connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_principale.bind((hote, port))
 connexion_principale.listen(1)
-print "Le serveur écoute à présent sur le port", port
+print "Le serveur ecoute a présent sur le port", port
 
 connexion_avec_client, infos_connexion = connexion_principale.accept()
 
@@ -14,9 +15,9 @@ msg_recu = b""
 while msg_recu != b"fin":
     msg_recu = connexion_avec_client.recv(1024)
 
-    print(msg_recu.decode())
+    print msg_recu.decode()
     connexion_avec_client.send(b"5 / 5")
 
-print("Fermetures de connexions")
+print "Fermetures de connexions"
 connexion_avec_client.close()
 connexion_principale.close()
