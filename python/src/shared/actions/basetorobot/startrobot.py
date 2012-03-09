6ba856c39554b8f328a65d943998c566473e3f85
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-03-09 10:43:54 -0500

    StartRobot action contains obstacle positions

--- python/src/shared/actions/basetorobot/startrobot.py
+++ python/src/shared/actions/basetorobot/startrobot.py
@@ -1,2 +1,6 @@
 class StartRobot():
-    pass
+    def __init__(self, obstacle_1_x, obstacle_1_y, obstacle_2_x, obstacle_2_y):
+        self.obstacle_1_x = obstacle_1_x
+        self.obstacle_1_y = obstacle_1_y
+        self.obstacle_2_x = obstacle_2_x
+        self.obstacle_2_y = obstacle_2_y
