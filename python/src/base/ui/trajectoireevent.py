d97fcc2922db52b0ff63ffa3570435da05f011e2
David <david.letourneau.3@ulaval.ca>
2012-03-22 19:04:58 -0400

    David 2012-03-22 19:04

---
+++ python/src/base/ui/trajectoireevent.py
@@ -0,0 +1,11 @@
+class TrajectoireEvent:
+    _handlers = []
+
+    @staticmethod
+    def addHandler(handler):
+        LogEvent._handlers.append(handler)
+
+    @staticmethod
+    def fire(*args, **keywargs):
+        for handler in LogEvent._handlers:
+            handler(*args, **keywargs)
---
+++ python/src/shared/actions/robottobase/sendtrajectoire.py
@@ -0,0 +1,5 @@
+class SendTrajectoire:
+    def __init__(self, listex=[], listey=[], LongueurListe= 0):
+        self.listex= listex
+        self.listey = listey
+        self.LongueurListe = LongueurListe
