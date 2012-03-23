3813773ca35c0d13bd6c23db835dca1d652f7055
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-03-22 21:00:30 -0400

    Test de directions dans le UI

--- python/src/robot/ai/states/advancestate.py
+++ python/src/robot/ai/states/advancestate.py
@@ -1,10 +1,19 @@
+import time
 from python.src.robot.ai.states.state import State
 from python.src.robot.ai.statecontroller import StateController
+from python.src.robot.sendevent import SendEvent
+from python.src.shared.actions.robottobase.sendpose import SendPose
 
 class AdvanceState(State):
     def __init__(self, distanceInCentimeters):
         self.__distanceInCentimeters = distanceInCentimeters
 
     def run(self):
+        SendEvent.send(SendPose(100, 50, 0))
+        time.sleep(3)
+        SendEvent.send(SendPose(200, 70, 30))
+        time.sleep(3)
+        SendEvent.send(SendPose(200, 70, 90))
+
         StateController.instance.endMainLoop()
 
