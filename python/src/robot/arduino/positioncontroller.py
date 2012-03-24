2372462547360ac534a75f189707b1d797511642
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-03-24 14:09:45 -0400

    Field testing for rotation

--- python/src/robot/arduino/positioncontroller.py
+++ python/src/robot/arduino/positioncontroller.py
@@ -14,6 +14,35 @@ class PositionController:
         ser.write('V5.')
         ser.write('M.')
 
-if __name__=="__main__":
-    positionctrl = PositionController()
-    positionctrl.advance(10)
+        self.__checkIfOperationIsOver(ser)
+
+
+    def rotate(self, angleInDegrees):
+        inversedAngle = -angleInDegrees #this is because the angles in the python code are positive in counter-clockwise direction,
+                                        #whereas angles in the arduino are positive in clockwise direction.
+
+        ser = Serial('/dev/ttyACM0', 115200)
+
+        time.sleep(1)
+        ser.setDTR(level=0)
+        time.sleep(1)
+
+        ser.write('R' + str(inversedAngle) + '.')
+
+        self.__checkIfOperationIsOver(ser)
+
+    def __checkIfOperationIsOver(self, ser):
+
+        operationOver = False
+
+        while not operationOver:
+            time.sleep(0.1)
+            line = ser.readline()
+            print line
+            if str(line) == "over\n":
+                operationOver = True
+
+if __name__== '__main__':
+    posctrl = PositionController()
+    posctrl.advance(10)
+    posctrl.rotate(90)
