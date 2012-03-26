21e2c6aaf2efa6f99809b204a22c50147c5b560e
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-03-26 12:30:35 -0400

    Small modifications

--- python/src/robot/ai/landmarks.py
+++ python/src/robot/ai/landmarks.py
@@ -1,6 +1,3 @@
 class Landmarks:
-    ARTAG1 = Landmarks((0, 1))
-    ARTAG2 = Landmarks((2, 3))
-
-    def __init__(self, position):
-        self.position = position
+    ARTAG1 = (0, 1)
+    ARTAG2 = (2, 3)
--- python/unittests/robot/ai/test_landmarks.py
+++ python/unittests/robot/ai/test_landmarks.py
@@ -3,4 +3,4 @@ from python.src.robot.ai.landmarks import Landmarks
 
 class TestLandmarks(unittest.TestCase):
     def test_artag1(self):
-        self.assertEqual((0,1), Landmarks.ARTAG1.position)
+        self.assertEqual((0,1), Landmarks.ARTAG1)
