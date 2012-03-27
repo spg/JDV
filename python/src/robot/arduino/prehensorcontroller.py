1b433f52dc60958a36f5524444d99e8e8524d921
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-03-27 14:44:53 -0400

    Refactored controllers. Added prehensor controller

---
+++ python/src/robot/arduino/prehensorcontroller.py
@@ -0,0 +1,19 @@
+from python.src.robot.arduino.arduinointerface import ArduinoInterface
+
+class PrehensorController:
+    def __init__(self):
+        self.arduinoInterface = ArduinoInterface()
+
+    def dropPrehensor(self):
+        ser = self.arduinoInterface.connect()
+
+        ser.write('PD.')
+
+        self.arduinoInterface.checkIfOperationIsOver(ser)
+
+    def raisePrehensor(self):
+        ser = self.arduinoInterface.connect()
+
+        ser.write('PR.')
+
+        self.arduinoInterface.checkIfOperationIsOver(ser)
