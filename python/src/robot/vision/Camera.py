38790e537a583c759bf65e09e25e376bb386ba19
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-03-28 22:50:41 -0400

    Interface pour la segmentation d'image

--- python/src/robot/vision/Camera.py
+++ python/src/robot/vision/Camera.py
@@ -3,6 +3,18 @@ import cv
 
 
 class Camera:
+    def getDrawingContour(self):
+        #ton code ici...
+        # ...
+        # ...
+        # ton code termine, tu retournes les valeurs suivantes:
+
+        points = [(3,5), (0,4), (100,345)] # par exemple
+
+        zoneWidth = 600 # la largeur de la zone carrée qui contient l'image
+
+        return points, zoneWidth
+
 
     def __init__(self):
         self.camera = cv.CaptureFromCAM(0)
