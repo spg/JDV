3b0c456fc164864febb1ba948e799f24fd66b410
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-04-03 14:14:40 -0400

    Ready for drawing

--- python/src/robot/ai/states/beginstate.py
+++ python/src/robot/ai/states/beginstate.py
@@ -1,33 +1,76 @@
+from __future__ import division
+
 import time
 from python.src.robot.ai.statecontroller import StateController
 from python.src.robot.ai.states.state import State
+from python.src.robot.arduino.prehensorcontroller import PrehensorController
 from python.src.robot.arduino.robotmover import RobotMover
 from python.src.robot.robot import Robot
+from python.src.robot.sendevent import SendEvent
 from python.src.robot.terrain import Terrain
+from python.src.robot.util.pointscloudscaler import PointsCloudOperations
+from python.src.robot.vision.Camera import Camera
+from python.src.shared.actions.robottobase.senddesssin import SendDessin
 
 class BeginState(State):
     def __init__(self, obstacle1, obstacle2):
         Terrain.OBSTACLE_1 = obstacle1
         Terrain.OBSTACLE_2 = obstacle2
 
+        self.robotMover = RobotMover()
+
     def run(self):
         Robot.setCurrentPose((207.8, 22.5, 90))
 
         print "running..."
-        robotMover = RobotMover()
 
         print "doing snake movement 1..."
-        robotMover.doSnakeMovement(Terrain.FIGURE_6_FACE, 270)
+        self.robotMover.doSnakeMovement(Terrain.FIGURE_5_FACE, 180)
         print "snake movement 1 over!"
-
-        Robot.setCurrentPose((Terrain.FIGURE_6_FACE[0], Terrain.FIGURE_6_FACE[1], 270))
 
         time.sleep(3)
 
-        print "doing snake movement 2..."
-        robotMover.doSnakeMovement(Terrain.DRAWING_ZONE_CENTER, 270)
-        print "snake movement 2 over!"
-
-        #Robot.setCurrentPose((Terrain.DRAWING_ZONE_CENTER[0], Terrain.FIGURE_5_FACE[1], 180))
+        self.doDrawing()
 
         StateController.instance.endMainLoop()
+
+    def doDrawing(self):
+
+        print "Creating camera..."
+        cam = Camera()
+
+        print "Extracting points..."
+        drawingCountour = cam.getDrawingContour()
+
+        points = drawingCountour[0]
+        size = drawingCountour[1]
+
+        print "Extracted points: "
+        print points
+        print "Size: "
+        print size
+
+        drawingZoneSide = 60 # dimension in CM
+        scaleFactor = drawingZoneSide / size
+        
+        scaledPoints = PointsCloudOperations.scale(points, scaleFactor)
+
+        SendEvent.send(SendDessin(scaledPoints))
+
+        movedPoints = PointsCloudOperations.move(scaledPoints, 144.8, 25.5)
+
+        print "going to first drawing point"
+        self.robotMover.doSnakeMovement(movedPoints[0], 270)
+        print "arrived at first drawing point!"
+
+        prehensorController = PrehensorController()
+        prehensorController.dropPrehensor()
+
+        self.robotMover.doShuffleMovement(movedPoints)
+
+        prehensorController.raisePrehensor()
+
+
+
+
+
--- python/src/robot/arduino/robotmover.py
+++ python/src/robot/arduino/robotmover.py
@@ -3,6 +3,7 @@ from python.src.robot.arduino.positioncontroller import PositionController
 from python.src.robot.pathplanning import advance, rotate, shuffle
 from python.src.robot.pathplanning.pathbuilder import PathBuilder
 from python.src.robot.pathplanning.rotate import Rotate
+from python.src.robot.pathplanning.shufflemovementplanner import ShuffleMovementPlanner
 from python.src.robot.pathplanning.snakemovementplanner import SnakeMovementPlanner
 from python.src.robot.robot import Robot
 
@@ -19,15 +20,35 @@ class RobotMover:
             elif move.__module__ == shuffle.__name__:
                 self.positionController.shuffle(move.distanceInCentimeters, move.relativeAngleInDegrees)
 
-    def doSnakeMovement(self, destination, finalAbsoluteAngle):
+    def __buildPath(self, destination):
         pathBuilder = PathBuilder()
         path = pathBuilder.build(destination)
         del path[0]
         print path
 
+        return path
+
+    def doSnakeMovement(self, destination, finalAbsoluteAngle):
+        path = self.__buildPath(destination)
+
         moves = SnakeMovementPlanner().planMovement(Robot.getCurrentPose(), path, finalAbsoluteAngle)
 
+        print "about to execute snake moves..."
         print moves
-        print "about to execute moves..."
 
         self.executeMoves(moves)
+
+        Robot.setCurrentPose((destination[0], destination[1], finalAbsoluteAngle))
+
+    def doShuffleMovement(self, path):
+        del path[0] # path[0] is the current robot's pose
+
+        moves = ShuffleMovementPlanner().planMovement(Robot.getCurrentPose(), path)
+
+        print "about to execute shuffle moves..."
+        print moves
+
+        self.executeMoves(moves)
+
+        lastNode = path[len(path) - 1]
+        Robot.setCurrentPose((lastNode[0], lastNode[1], 270))
