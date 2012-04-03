c392c5a5541fe58d90039698b7d3a41d9cc9d8fb
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-04-03 13:26:13 -0400

    More tests for cloud scaling

--- python/unittests/robot/util/test_pointscloudscaler.py
+++ python/unittests/robot/util/test_pointscloudscaler.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import unittest
 from python.src.robot.util.pointscloudscaler import PointsCloudScaler
 
@@ -11,3 +13,13 @@ class TestPointsCloudScaler(unittest.TestCase):
         cloud = [(1,1), (2,2), (2,0), (0,0)]
 
         self.assertEqual([(2,2), (4,4), (4,0), (0,0)], PointsCloudScaler.scalePointsCloud(cloud, 2))
+
+    def test_scalePointsCloud_simpleCase_3(self):
+        cloud = [(100, 100), (250,80), (400,400), (0,0)]
+
+        self.assertEqual([(15,15), (37.5, 12), (60,60), (0,0)], PointsCloudScaler.scalePointsCloud(cloud, 60/400))
+
+    def test_scalePointsCloud_simpleCase_4(self):
+        cloud = [(103, 90), (254,78), (691,690), (3,4)]
+
+        self.assertEqual([(8.24, 7.2), (20.32, 6.24), (55.28, 55.2), (0.24, 0.32)], PointsCloudScaler.scalePointsCloud(cloud, 60/750))
