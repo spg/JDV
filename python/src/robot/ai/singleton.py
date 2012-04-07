853d207206087d2a232c9cce6748c96c3b1ecde1
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-04-07 13:12:05 -0400

    Dead code

--- python/src/robot/ai/singleton.py
+++
@@ -1,9 +0,0 @@
-from python.src.robot.ai.singletonaccessexception import SingletonAccessException
-
-class Singleton:
-    instance = None
-    def __init__( self ):
-        if Singleton.instance:
-            raise SingletonAccessException()
-        print "singleton created"
-        Singleton.instance = self
--- python/src/robot/ai/states/captureimagestate.py
+++
@@ -1,9 +0,0 @@
-from python.src.robot.ai.statecontroller import StateController
-from python.src.robot.ai.states.drawstate import DrawState
-from python.src.robot.ai.states.state import State
-
-class CaptureImageState(State):
-    def run(self):
-        #capture image
-
-        StateController.instance.setCurrentState(DrawState())
