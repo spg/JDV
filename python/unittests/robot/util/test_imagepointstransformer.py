9aed5a5df120aac08f42d056ea35ae39af01911e
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-04-08 15:35:42 -0400

    Using codes from manchester signal interpreter

--- python/unittests/robot/util/test_imagepointstransformer.py
+++ python/unittests/robot/util/test_imagepointstransformer.py
@@ -1,4 +1,5 @@
 import unittest
+from python.src.robot.arduino.manchestersignalinterpreter import ManchesterSignalInterpreter
 from python.src.robot.util.imagepointstransformer import ImagePointsTransformer
 
 class TestImagePointsTransformer(unittest.TestCase):
@@ -8,39 +9,39 @@ class TestImagePointsTransformer(unittest.TestCase):
     def test_north_4(self):
         pointsFromImage = ([(100, 100), (350, 100), (100, 400)], 500)
 
-        transformedPoints = self.imagePointsTransformer.transform(pointsFromImage, ImagePointsTransformer.NORTH,
-            ImagePointsTransformer.SCALE_4)
+        transformedPoints = self.imagePointsTransformer.transform(pointsFromImage, ManchesterSignalInterpreter.NORTH,
+            ManchesterSignalInterpreter.FACTOR_4)
 
         self.assertEqual([(12, 12), (42, 12), (12, 48)], transformedPoints)
 
     def test_east_4(self):
         pointsFromImage = ([(100, 100), (350, 100), (100, 400)], 500)
 
-        transformedPoints = self.imagePointsTransformer.transform(pointsFromImage, ImagePointsTransformer.EAST,
-            ImagePointsTransformer.SCALE_4)
+        transformedPoints = self.imagePointsTransformer.transform(pointsFromImage, ManchesterSignalInterpreter.EAST,
+            ManchesterSignalInterpreter.FACTOR_4)
 
         self.assertEqual([(12, 48), (12, 18), (48, 48)], transformedPoints)
 
     def test_west_4(self):
         pointsFromImage = ([(100, 100), (350, 100), (100, 400)], 500)
 
-        transformedPoints = self.imagePointsTransformer.transform(pointsFromImage, ImagePointsTransformer.WEST,
-            ImagePointsTransformer.SCALE_4)
+        transformedPoints = self.imagePointsTransformer.transform(pointsFromImage, ManchesterSignalInterpreter.WEST,
+            ManchesterSignalInterpreter.FACTOR_4)
 
         self.assertEqual([(48, 12), (48, 42), (12, 12)], transformedPoints)
 
     def test_west_2(self):
         pointsFromImage = ([(100, 100), (350, 100), (100, 400)], 500)
 
-        transformedPoints = self.imagePointsTransformer.transform(pointsFromImage, ImagePointsTransformer.WEST,
-            ImagePointsTransformer.SCALE_2)
+        transformedPoints = self.imagePointsTransformer.transform(pointsFromImage, ManchesterSignalInterpreter.WEST,
+            ManchesterSignalInterpreter.FACTOR_2)
 
         self.assertEqual([(39, 21), (39, 36), (21, 21)], transformedPoints)
 
     def test_west_2_square(self):
         pointsFromImage = ([(100, 400), (100, 100), (400, 100), (400,400)], 500)
 
-        transformedPoints = self.imagePointsTransformer.transform(pointsFromImage, ImagePointsTransformer.WEST,
-            ImagePointsTransformer.SCALE_2)
+        transformedPoints = self.imagePointsTransformer.transform(pointsFromImage, ManchesterSignalInterpreter.WEST,
+            ManchesterSignalInterpreter.FACTOR_2)
 
         self.assertEqual([(21, 21), (39, 21), (39, 39), (21, 39)], transformedPoints)
