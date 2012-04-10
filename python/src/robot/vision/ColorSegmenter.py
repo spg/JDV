aa235fc342dbb2e93d8a439607fc3cac88282f06
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-04-10 13:12:43 -0400

    Vision

---
+++ python/src/robot/vision/ColorSegmenter.py
@@ -0,0 +1,59 @@
+﻿
+import cv2.cv as cv
+
+class ColorSegmenter:
+    blue = 0
+    orange = 1
+    red = 2
+    green = 3
+    white = 4
+
+    def __init__(self):
+        self.lowerTreshold = []
+        self.upperTreshold = []
+
+        #Tresholds for the color blue
+        self.lowerTreshold.append(cv.Scalar(75, 0,0))
+        self.upperTreshold.append(cv.Scalar(140,255,255))
+
+        #Tresholds for the color orange
+        self.lowerTreshold.append(cv.Scalar(0,100,100))
+        self.upperTreshold.append(cv.Scalar(12,255,255))
+
+        #Tresholds for the color red
+        self.lowerTreshold.append(cv.Scalar(0,100,100))
+        self.upperTreshold.append(cv.Scalar(4,255,255))
+
+        #Tresholds for the color green
+        self.lowerTreshold.append(cv.Scalar(40,100,50))
+        self.upperTreshold.append(cv.Scalar(80,255,255))
+
+        #Tresholds for the color white
+        self.lowerTreshold.append(cv.Scalar(0,0,250))
+        self.upperTreshold.append(cv.Scalar(255,255,255))
+
+    def segmentImageByColor(self, sourceImage, color):
+        if color >= 0 and color <= 5:
+            hsvImage = self.convertImageToHSV(sourceImage)
+            imgTresh = cv.CreateImage(cv.GetSize(sourceImage), 8, 1)
+            imgTresh = self.treshHSVImageByGivenColor(hsvImage, imgTresh, color)
+            imgTresh = self.reduceNoiseOnTreshedImage(imgTresh)
+            cv.SaveImage("segmentationResult.jpg", imgTresh)
+            return imgTresh
+
+    def convertImageToHSV(self, sourceImage):
+        hsvImage = cv.CreateImage(cv.GetSize(sourceImage), 8, 3)
+        cv.CvtColor(sourceImage, hsvImage, cv.CV_BGR2HSV)
+        return hsvImage
+
+    def treshHSVImageByGivenColor(self, hsvImage, imgTresh, color):
+        cv.SaveImage("BeforeTresh.jpg", hsvImage)
+        cv.InRangeS(hsvImage, self.lowerTreshold[color], self.upperTreshold[color], imgTresh)
+        cv.SaveImage("AfterTresh.jpg", imgTresh)
+        return imgTresh
+
+    def reduceNoiseOnTreshedImage(self, imgTresh):
+        cv.Erode(imgTresh, imgTresh, None, 3)
+        cv.Dilate(imgTresh, imgTresh, None, 4)
+        return imgTresh
+
---
+++ python/src/robot/vision/SideDetector.py
@@ -0,0 +1,50 @@
+﻿from ColorSegmenter import ColorSegmenter
+import cv2.cv as cv
+
+class SideDetector:
+    WEST_SIDE = 0
+    EAST_SIDE = 1
+        
+    def detectVisibleSide(self, image):
+        imageCopy = cv.CloneImage(image)
+        colorSegmenter = ColorSegmenter()
+        greenSegmentation = colorSegmenter.segmentImageByColor(imageCopy, colorSegmenter.green)
+        contours = self.__findContoursInPicture__(greenSegmentation)
+        middleHeight = self.__getMiddleHeightOfImage__(imageCopy)
+        contours = self.__removeContoursOnBottom__(contours, middleHeight)
+        if len(contours) > 0:
+            return self.WEST_SIDE
+        else:
+            return self.EAST_SIDE
+
+    def __findContoursInPicture__(self, binaryImage):
+        storage = cv.CreateMemStorage()
+        contours = cv.FindContours(binaryImage,
+            storage,cv.CV_RETR_LIST,
+            cv.CV_CHAIN_APPROX_SIMPLE)
+        storage = cv.CreateMemStorage()
+        if len(contours) > 0:
+            contours = cv.ApproxPoly (contours,
+                storage,
+                cv.CV_POLY_APPROX_DP, 3, 1)
+        return contours
+    
+    def __getMiddleHeightOfImage__(self, image):
+        size = cv.GetSize(image)
+        return size[1]/2
+    
+    def __removeContoursOnBottom__(self, contours, middleHeight):
+        filteredContours = []
+        _contour = contours
+        while _contour is not None:
+            nbPoints = 0
+            if len(_contour) > 0:
+                for point in _contour:
+                    if point[1] < middleHeight:
+                        nbPoints += 1
+                if nbPoints > 0 and cv.ContourArea(_contour) > 1000:
+                    filteredContours.append(_contour)
+                _contour = _contour.h_next()
+            else:
+                _contour = None
+        return filteredContours
