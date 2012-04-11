2494b72e9b4adac37aa4ed02b3899ee6f6822762
unknown <LiberT@LIBERTU-49JBVV8.(none)>
2012-04-11 00:12:21 -0400

    modif vision

--- python/src/robot/vision/cameraAccessor.py
+++ python/src/robot/vision/cameraAccessor.py
@@ -4,16 +4,20 @@ import numpy
 class CameraAccessor:
 
     def __init__(self):
+        print "CameraAccessor - initialiseCamera begin"
         self.__initialiseCamera__()
         self.__getCalibrationParameters__()
         self.__initialiseUndistortMap__()
+        print "CameraAccessor - initialiseCamera end"
 
     def getFrame(self, undistort):
+        print "CameraAccessor - getFrame begin"
         #cv.SetCaptureProperty(self.camera, cv.CV_CAP_PROP_FRAME_HEIGHT, 1200)
         #cv.SetCaptureProperty(self.camera, cv.CV_CAP_PROP_FRAME_WIDTH, 1600)
         frame = cv.QueryFrame(self.camera)
         if undistort == True:
             cv.Remap(frame, frame, self.mapx, self.mapy)
+        print "CameraAccessor - getFrame end"
         return frame
 
     def __getCalibrationParameters__(self):
@@ -28,10 +32,12 @@ class CameraAccessor:
         #cv.SetCaptureProperty(self.camera, cv.CV_CAP_PROP_FORMAT, cv.IPL_DEPTH_32F)
 
     def __initialiseUndistortMap__(self):
+        print "CameraAccessor - initialiseUndistortMap begin"
         testImage = cv.QueryFrame(self.camera) 
         #print "cameraAccessor: initialiseUndistortMap - ",  type(testImage)
         self.photoSize = cv.GetSize(testImage)
         self.mapx = cv.CreateImage(self.photoSize, cv.IPL_DEPTH_32F, 1)
         self.mapy = cv.CreateImage(self.photoSize, cv.IPL_DEPTH_32F, 1)
         cv.InitUndistortMap(cv.fromarray(self.intrinsecParameters), cv.fromarray(self.distortionParameter), self.mapx, self.mapy)
+        print "CameraAccessor - initialiseUndistortMap end"
 
