106dccbc8e4de6ff13614252a5b8353f5db8aa7a
Cauthom <alexandre.hache1@gmail.com>
2012-04-11 04:26:00 -0300

    Update python/src/robot/vision/cameraAccessor.py

--- python/src/robot/vision/cameraAccessor.py
+++ python/src/robot/vision/cameraAccessor.py
@@ -4,18 +4,18 @@ import numpy
 class CameraAccessor:
 
     def __init__(self):
-        print "CameraAccessor - initialiseCamera begin"
+        #print "CameraAccessor - initialiseCamera begin"
         self.__initialiseCamera__()
         self.__getCalibrationParameters__()
         self.__initialiseUndistortMap__()
-        print "CameraAccessor - initialiseCamera end"
+        #print "CameraAccessor - initialiseCamera end"
 
     def getFrame(self, undistort):
-        print "CameraAccessor - getFrame begin"
+        #print "CameraAccessor - getFrame begin"
         frame = cv.QueryFrame(self.camera)
         if undistort == True:
             cv.Remap(frame, frame, self.mapx, self.mapy)
-        print "CameraAccessor - getFrame end"
+        #print "CameraAccessor - getFrame end"
         return frame
 
     def __getCalibrationParameters__(self):
@@ -23,18 +23,18 @@ class CameraAccessor:
         self.distortionParameter = numpy.load("vision/distortion.npy")
 
     def __initialiseCamera__(self):
-        print "Initialising camera..."
+        #print "Initialising camera..."
         self.camera = cv.CaptureFromCAM(-1)
         frame = self.getFrame(False)
-        print cv.GetSize(frame)
+        #print cv.GetSize(frame)
 
     def __initialiseUndistortMap__(self):
-        print "CameraAccessor - initialiseUndistortMap begin"
+        #print "CameraAccessor - initialiseUndistortMap begin"
         testImage = cv.QueryFrame(self.camera) 
-        print "imageType = ", type(testImage)
+        #print "imageType = ", type(testImage)
         self.photoSize = cv.GetSize(testImage)
         self.mapx = cv.CreateImage(self.photoSize, 32, 1)
         self.mapy = cv.CreateImage(self.photoSize, 32, 1)
         cv.InitUndistortMap(cv.fromarray(self.intrinsecParameters), cv.fromarray(self.distortionParameter), self.mapx, self.mapy)
-        print "CameraAccessor - initialiseUndistortMap end"
+        #print "CameraAccessor - initialiseUndistortMap end"
 
