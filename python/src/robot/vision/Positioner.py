a2077eed79b91ef9d1b25932a6ea15eba91785bb
unknown <LiberT@LIBERTU-49JBVV8.(none)>
2012-04-10 23:30:03 -0400

    modif vision

--- python/src/robot/vision/Positioner.py
+++ python/src/robot/vision/Positioner.py
@@ -6,6 +6,7 @@ from python.src.robot.terrain import Terrain
 
 class Positionner:
     def __init__(self):
+        print "Positionner - init begin"
         transformationMatrix = np.load("vision/extrinsec.npy")
         self.m11 = transformationMatrix[0][0]
         self.m12 = transformationMatrix[0][1]
@@ -22,8 +23,10 @@ class Positionner:
         self.CORNER_LENGHT = 17
         self.TABLE_LENGHT = 229.8
         self.TABLE_HEIGHT = 111
+        print "Positionner - init end"
 
     def getCurrentPose(self, point1, point2, corner):
+        print "Positionner - get current pose begin"
         cameraPoint1 = self.getPositionRelativeToCamera(point1)
         cameraPoint2 = self.getPositionRelativeToCamera(point2)
         robotPoint1 = self.convertPointToRobotCoordinates(cameraPoint1)
@@ -34,23 +37,29 @@ class Positionner:
         offsetAngle = self.calculateAngleBetweenPoints(robotPoint1, robotPoint2, corner)
         robotAngle = defaultAngle +  offsetAngle
         x,y = self.getRobotPosition(robotPoint1, robotPoint2, corner)
+        print "Positionner - get current pose end"
         return x, y, robotAngle
 
     def getPositionRelativeToCamera(self, point):
+        print "Positionner - getPositionRelativeToCam begin"
         u = point[0]
         v = point[1]
         s = (self.m11 - u*self.m31)*(self.m22 - v*self.m32) + (self.m12 - u*self.m32)*(-self.m21 + v*self.m31)
         X = ((-self.m14 + u*self.m34)*(self.m22 - v*self.m32) - (self.m24 - v*self.m34)*(-self.m12 + u*self.m32)) / s
         Y = ((-self.m14 + u*self.m34)*(-self.m21 + v*self.m31) - (self.m24 - v*self.m34)*(self.m11 - u*self.m31)) / s
+        print "Positionner - getPositionRelativeToCam end"
         return (X,Y)
 
     def convertPointToRobotCoordinates(self, point):
+        print "Positionner - robotCoord begin"
         yDistanceFromPicturOrigin = 68.58
         newX = point[0]
         newY = -1*(point[1] - yDistanceFromPicturOrigin)
+        print "Positionner - robotCoord end"
         return (newX, newY)
 
     def getRealWorldCoordinates(self, point1, point2, corner):
+        print "Positionner - rwc begin"
         if corner == CornerDetector.WEST_BLUE_CORNER:
             rwc1 = Terrain.CORNER_BLUE_WEST_LEFT_EDGE
             rwc2 = Terrain.CORNER_BLUE_WEST_RIGHT_EDGE
@@ -63,15 +72,19 @@ class Positionner:
         elif corner == CornerDetector.WEST_BLUE_CORNER:
             rwc1 = Terrain.CORNER_BLUE_EAST_LEFT_EDGE
             rwc2 = Terrain.CORNER_BLUE_EAST_RIGHT_EDGE
+        print "Positionner - rwc end"
         return rwc1, rwc2
 
     def calculateAngleBetweenPoints(self, point1, point2, corner):
+        print "Positionner - calculateangleBetweenPoints begin"
         deltaX = point2[0] - point1[0]
         deltaY = point2[1] - point1[1]
         theta = math.atan(deltaY/deltaX)
+        print "Positionner - calculateangleBetweenPoints end"
         return theta*360/(2* math.pi)
 
     def getCornerAngle(self, corner):
+        print "Positionner - getCornerAngle begin"
         if corner == CornerDetector.WEST_BLUE_CORNER:
             return -135
         elif corner == CornerDetector.WEST_ORANGE_CORNER:
@@ -80,12 +93,16 @@ class Positionner:
             return 45
         elif corner == CornerDetector.EAST_BLUE_CORNER:
             return -45
+        print "Positionner - getCornerAngle end"
 
 
     def getDistanceBetweenPoints(self, point1, point2):
+        print "Positionner - getDistanceBetweenPoints begin"
+        print "Positionner - getDistanceBetweenPoints end"
         return math.sqrt(math.pow(point2[0]- point1[0], 2) + math.pow(point2[1] - point1[1], 2))
 
     def getRobotPosition(self, point1, point2, corner):
+        print "Positionner - getRobotPosition begin"
         sideLeft = self.getDistanceBetweenPoints(point1, (0,0))
         sideRight = self.getDistanceBetweenPoints(point2, (0,0))
         angleLeft, angleRight = self.getAnglesByCosinusLaw(sideLeft, sideRight)
@@ -111,12 +128,15 @@ class Positionner:
         elif corner == CornerDetector.EAST_BLUE_CORNER:
             x = self.TABLE_LENGHT - rightPosition
             y = self.TABLE_HEIGHT - leftPosition
+        print "Positionner - getRobotPosition end"
         return x, y
 
 
     def getAnglesByCosinusLaw(self, sideLeft, sideRight):
+        print "Positionner - cosLaw begin"
         cosRight = (math.pow(sideLeft, 2) - math.pow(sideRight,2) - math.pow(self.CORNER_LENGHT,2)) / (-2*sideRight*self.CORNER_LENGHT)
         angleRight = math.acos(cosRight)
         cosLeft = (math.pow(sideRight, 2) - math.pow(sideLeft,2) - math.pow(self.CORNER_LENGHT,2)) / (-2*sideLeft*self.CORNER_LENGHT)
         angleLeft = math.acos(cosLeft)
+        print "Positionner - cosLaw end"
         return math.degrees(angleLeft), math.degrees(angleRight)
