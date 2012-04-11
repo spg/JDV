2ef3e9530946db8f633e3fb8aef00ee038166da8
unknown <LiberT@LIBERTU-49JBVV8.(none)>
2012-04-11 19:58:40 -0400

    Positionnement

--- python/src/robot/vision/ColorSegmenter.py
+++ python/src/robot/vision/ColorSegmenter.py
@@ -13,8 +13,8 @@ class ColorSegmenter:
         self.upperTreshold = []
 
         #Tresholds for the color blue
-        self.lowerTreshold.append(cv.Scalar(110, 0,0))
-        self.upperTreshold.append(cv.Scalar(165,255,255))
+        self.lowerTreshold.append(cv.Scalar(80, 0,0))
+        self.upperTreshold.append(cv.Scalar(170,255,255))
 
         #Tresholds for the color orange
         self.lowerTreshold.append(cv.Scalar(0,100,100))
