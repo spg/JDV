98e84cb99e02958ad5966e3599c540d59a239ca7
unknown <LiberT@LIBERTU-49JBVV8.(none)>
2012-04-10 22:43:33 -0400

    ajustement du test de pose

--- python/src/robot/poseTest.py
+++ python/src/robot/poseTest.py
@@ -4,5 +4,7 @@ camera = Camera()
 try:
     print "Determining current pose..."
     x, y, theta = camera.getCurrentPose()
+    print "current position = ", x , y
+    print "angle = ", theta
 except:
     print "Couldn't estimate current pose. Please try again"
