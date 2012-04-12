1232d0190cd3e56bc42c8ed288ed705307a3405b
unknown <LiberT@LIBERTU-49JBVV8.(none)>
2012-04-12 01:21:16 -0400

    Allocation et liberation de la cam lors de la capture d'images

--- python/src/robot/vision/cameraAccessor.py
+++ python/src/robot/vision/cameraAccessor.py
@@ -5,18 +5,20 @@ class CameraAccessor:
 
     def __init__(self):
         #print "CameraAccessor - initialiseCamera begin"
-        self.__initialiseCamera__()
+        #self.__initialiseCamera__()
         self.__getCalibrationParameters__()
         self.__initialiseUndistortMap__()
         #print "CameraAccessor - initialiseCamera end"
 
     def getFrame(self, undistort):
-        #print "CameraAccessor - getFrame begin"
+        self.camera = cv.CaptureFromCAM(-1)
+        print "CameraAccessor: Allocating Camera"
         for i in range(1,5):
             frame = cv.QueryFrame(self.camera)
         if undistort == True:
             cv.Remap(frame, frame, self.mapx, self.mapy)
-        #print "CameraAccessor - getFrame end"
+        cv.ReleaseCapture(self.camera)
+        print "CameraAccessor: Releasing camera"
         return frame
 
     def __getCalibrationParameters__(self):
@@ -26,8 +28,8 @@ class CameraAccessor:
     def __initialiseCamera__(self):
         #print "Initialising camera..."
         self.camera = cv.CaptureFromCAM(-1)
-        frame = self.getFrame(False)
-        print "Frame size = ", cv.GetSize(frame)
+        #frame = self.getFrame(False)
+        #print "Frame size = ", cv.GetSize(frame)
 
     def __initialiseUndistortMap__(self):
         #print "CameraAccessor - initialiseUndistortMap begin"
