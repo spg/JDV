47b4b268a01845e204e1cbf9c029e9c9242a5343
Simon-Pierre <simon-pierre@simonpierre-ubuntu-acer.(none)>
2012-04-11 20:29:20 -0400

    Position testing

--- python/fieldtests/ledtest.py
+++ python/fieldtests/ledtest.py
@@ -1,10 +1,42 @@
 import time
 from python.src.robot.arduino.ledcontroller import LedController
 
-ledController = LedController()
+class codeMorse:
+    SHORT = "short"
+    LONG = "long"
+    PAUSE = "pause"
 
-while True:
+    def __init__(self):
+        self.ledController = LedController()
 
-    ledController.turnLedOn()
-    time.sleep(0.2)
-    ledController.turnLedOff()
+    def doCode(self, signals):
+        for code in signals:
+            if code == codeMorse.SHORT:
+                self.__short()
+            elif code == codeMorse.LONG:
+                self.__long()
+            else:
+                self.__pause()
+
+    def __short(self):
+        self.ledController.turnLedOn()
+        time.sleep(0.1)
+        self.ledController.turnLedOff()
+
+    def __long(self):
+        self.ledController.turnLedOn()
+        time.sleep(1)
+        self.ledController.turnLedOff()
+
+    def __pause(self):
+        time.sleep(1)
+
+short = codeMorse.SHORT
+lonng = codeMorse.LONG
+pause = codeMorse.PAUSE
+
+signals = [short, lonng, short, short, pause, short, short, pause, lonng, short, lonng, short, pause, lonng, lonng, lonng,
+        pause, short, lonng, short, pause, lonng, short, pause, short]
+
+morseCodeer = codeMorse()
+morseCodeer.doCode(signals)
